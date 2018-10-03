0d668785b84f801fc6b232ab4e997ca7c371b08c
Thong Nguyen <thong.nguyen@hdwebsoft.co>
2018-10-03 10:44:17 +0700

    Singleton Pattern

---
+++ RWBlueLibrary/LibraryAPI.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryAPI.swift
+//  RWBlueLibrary
+//
+//  Created by hungdat1234 on 10/3/18.
+//  Copyright © 2018 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+
+final class LibraryAPI {
+  // 1
+  static let shared = LibraryAPI()
+  // 2
+  private init() {
+    
+  }
+}
